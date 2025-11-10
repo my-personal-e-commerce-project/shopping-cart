61c937c6a563a6459273dea265780fb4b8fb91b8
Mauro Caetano <lucianocaetano1999@gmail.com>
2025-11-10 10:14:38 -0300

    fixed some bugs

--- src/main/java/microservice/ecommerce/shopping_cart/ShoppingCartApplication.java
+++ src/main/java/microservice/ecommerce/shopping_cart/ShoppingCartApplication.java
@@ -1,11 +1,8 @@
 package microservice.ecommerce.shopping_cart;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-import jakarta.annotation.PostConstruct;
 
 @EnableFeignClients
 @SpringBootApplication
